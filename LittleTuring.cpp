c84a6068361184e69583e5af6d865c2aa5dce645
Adam Lastowka <adamlastowka@gmail.com>
2024-07-12 16:42:55 -0400

    updated example use

--- LittleTuring.cpp
+++ LittleTuring.cpp
@@ -4,17 +4,20 @@
 
 int main()
 {
-    TinyTuring::TuringMachine<int, char> beaver;
+    // Example Usage:
+    using namespace TinyTuring;
+
     // This is BB(5) -- ([Marxen & Buntrock, 1990])
     // https://bbchallenge.org/1RB1LC_1RC1RB_1RD0LE_1LA1LD_1RZ0LA
-    beaver = TinyTuring::from_std_format(std::string("1RB1LC_1RC1RB_1RD0LE_1LA1LD_1RZ0LA"));
+    TuringMachine<int, char> beaver{ from_std_format(std::string("1RB1LC_1RC1RB_1RD0LE_1LA1LD_1RZ0LA")) };
 
-    while (beaver.status == TinyTuring::TuringStatus::Running) {
+    while (beaver.status == TuringStatus::Running) {
         beaver.step();
-        //beaver.print_tape();
+        //beaver.print_tape(std::cout);
     }
 
+    // most bb.challenge.org machines halt via error (they go to state 'Z', which is never defined)
     auto status = beaver.step();
-    std::cout << (status == TinyTuring::TuringStatus::Halted ? "Halted" : "Errored");
+    std::cout << (status == TuringStatus::Halted ? "Halted" : "Errored");
     std::cout << " after " << beaver.get_steps_completed() << " iterations." << std::endl;
 }
